aa24533200817be0abca24e7776e365b906bb3ea
Scott Bromander <scott@primeacademy.io>
2016-09-07 20:36:19 -0400

    Object pool creation

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ObjectPool : MonoBehaviour {
+
+	public RecycleGameObject prefab;
+	public List<RecycleGameObject> poolInstances = new List<RecycleGameObject>();
+
+	private RecycleGameObject CreateInstance(Vector3 pos){
+		var clone = GameObject.Instantiate (prefab);
+		clone.transform.position = pos;
+		clone.transform.parent = transform;
+
+		poolInstances.Add (clone);
+
+		return clone;
+	}
+
+	public RecycleGameObject NextObject(Vector3 pos){
+		RecycleGameObject instance = null;
+
+		instance = CreateInstance (pos);
+		instance.Restart ();
+
+		return instance;
+	}
+}
