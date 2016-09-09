a9043c4473e5dbb6dd55e4398941908fb06f7f29
Scott Bromander <scott@primeacademy.io>
2016-09-08 20:09:40 -0400

    Creation and use of object pool object

--- Assets/Scripts/GameObjectUtil.cs
+++ Assets/Scripts/GameObjectUtil.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class GameObjectUtil {
+
+	private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool>();
 
 	public static GameObject Instantiate(GameObject prefab, Vector3 pos){
 		GameObject instance = null;
 
-		instance = GameObject.Instantiate (prefab);
-		instance.transform.position = pos;
+		var recycledScript = prefab.GetComponent<RecycleGameObject> ();
+		if (recycledScript != null) {
+			var pool = GetObjectPool (recycledScript);
+			instance = pool.NextObject (pos).gameObject;
+		} else {
+			instance = GameObject.Instantiate (prefab);
+			instance.transform.position = pos;
+		}
 
 		return instance;
 	}
@@ -22,6 +32,21 @@ public class GameObjectUtil {
 		}
 
 
+	}
+
+	private static ObjectPool GetObjectPool(RecycleGameObject reference){
+		ObjectPool pool = null;
+
+		if (pools.ContainsKey (reference)) {
+			pool = pools [reference];
+		} else {
+			var poolContainer = new GameObject (reference.gameObject.name + "ObjectPool");
+			pool = poolContainer.AddComponent<ObjectPool> ();
+			pool.prefab = reference;
+			pools.Add (reference, pool);
+		}
+
+		return pool;
 	}
 
 }
