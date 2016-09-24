8bb73024746adee7af1a588f121c4b808da72c77
Scott Bromander <scott@primeacademy.io>
2016-09-23 19:56:53 -0500

    basic manipulate time method

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -3,4 +3,11 @@ using System.Collections;
 
 public class TimeManager : MonoBehaviour {
 
+	public void ManipulateTime(float newTime, float duration){
+		if (Time.timeScale == 0) {
+			Time.timeScale = 0.1;
+		}
+	}
+
+
 }
